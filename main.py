5bb789863a215ae80fdaa7c5b2f20e788e66642e
aliaskar25 <aliaskar.isakov@gmail.com>
2020-07-12 06:14:00 +0600

    add wtf form and session variable "name"

--- main.py
+++ main.py
@@ -2,21 +2,52 @@ from flask import (Flask,
                    make_response, 
                    abort, 
                    redirect, 
-                   render_template, )
+                   render_template, 
+                   url_for, 
+                   flash, 
+                   session, )
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 from datetime import datetime
+import os
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", default="some secret key here")
+
+
+###### Init side apps #######
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route("/")
+########## Forms ############
+
+class NameForm(Form):
+    name = StringField("What is your name", validators=[Required()])
+    submit = SubmitField("Submit")
+
+########## Routes ###########
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("You have changed your name!")
+        session["name"], form.name.data = form.name.data, ""
+        return redirect(url_for("index"))
+    return render_template("index.html", form=form, 
+                           name=session.get("name"),
+                           current_time=datetime.utcnow())
 
 
 @app.route("/<word>")
